de5c892241b55bdbad451a64c7ae377bc50dc2a8
Ares <phutran121094@gmail.com>
2021-01-05 12:04:20 +0700

    update_deploy

--- src/services/API.ts
+++ src/services/API.ts
@@ -19,8 +19,11 @@ export type QuestionsState = Question & { answers: string[] }
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
+    // check answer
+    console.log("đây là kết quả",data.results);
     return data.results.map((question: Question) => ({
         ...question,
         answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+        
     }))
 };
